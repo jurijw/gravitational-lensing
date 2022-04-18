b93bb04e08ee3b13e0c095ac5bd4893545438c55
Jurij Waite <jurijlwaite@gmail.com>
2022-04-18 00:53:06 -0700

    Started a notebook for inverse ray shooting.

--- pysim/main.py
+++ pysim/main.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.random import default_rng
 
 
 def setup():
     x, y = np.linspace(-1, 1, 100), np.linspace(-1, 1, 100)
     xv, yv = np.meshgrid(x, y)
-    plt.imshow(alpha(xv, yv))
+    # plt.imshow(alpha(xv, yv))
+    # plt.show()
+
+    rng = default_rng()
+    X, Y = rng.standard_normal((10, 10))
+    plt.scatter(X, Y)
     plt.show()
 
 
