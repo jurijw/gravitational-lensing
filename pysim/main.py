e70bed99627c701b2961578a5e2570513f9d7cee
Jurij Waite <jurijlwaite@gmail.com>
2022-04-17 23:38:47 -0700

    Slightly modified main to plot 2D meshgrid.

--- pysim/main.py
+++ pysim/main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def setup():
+    x, y = np.linspace(-1, 1, 100), np.linspace(-1, 1, 100)
+    xv, yv = np.meshgrid(x, y)
+    plt.imshow(alpha(xv, yv))
+    plt.show()
 
 
-# Press the green button in the gutter to run the script.
+def alpha(x, y):
+    return x + y
+
+
+def main():
+    setup()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
